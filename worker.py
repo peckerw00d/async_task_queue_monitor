43007914a128d98a9e0f05b9636a3ef856ee5376
peckerw00d <balkarov05@gmail.com>
2025-07-31 17:17:23 +0300

    added Worker class with message queue processing

---
+++ worker.py
@@ -0,0 +1,75 @@
+import asyncio
+
+import aio_pika
+
+from models import Action, ResultMessage, TaskMessage
+
+
+class Worker:
+    def __init__(
+        self,
+        connection: aio_pika.abc.AbstractRobustConnection,
+        task_queue: str,
+        result_queue: str,
+    ):
+        self.connection = connection
+        self.task_queue = task_queue
+        self.result_queue = result_queue
+
+    async def start(self):
+        async with self.connection:
+            channel = await self.connection.channel()
+            await channel.set_qos(prefetch_count=10)
+
+            queue = await channel.declare_queue(self.task_queue, auto_delete=True)
+
+            async with queue.iterator() as queue_iter:
+                async for message in queue_iter:
+                    async with message.process():
+                        msg = TaskMessage.model_validate_json(message.body.decode())
+
+                        if msg.action == Action.WAIT:
+                            result = await self._process_wait_message(message=msg)
+                            await self._publish_task_results(
+                                message=msg, result=result, channel=channel
+                            )
+
+                        elif msg.action == Action.FAIL:
+                            result = await self._process_fail_message()
+                            await self._publish_task_results(
+                                message=msg, result=result, channel=channel
+                            )
+
+                        else:
+                            result = await self._process_ping_message()
+                            await self._publish_task_results(
+                                message=msg, result=result, channel=channel
+                            )
+
+    async def _publish_task_results(
+        self, message: TaskMessage, result: str, channel: aio_pika.abc.AbstractChannel
+    ):
+        result_message = ResultMessage(
+            task_id=message.task_id,
+            status="error" if message.action == Action.FAIL else "success",
+            result=result,
+            duration=(message.duration + 0.1) if message.action == Action.WAIT else 0.1,
+        )
+        await channel.default_exchange.publish(
+            aio_pika.Message(
+                body=result_message.model_dump_json().encode("utf-8"),
+                delivery_mode=2,
+                content_type="application/json",
+            ),
+            routing_key=self.result_queue,
+        )
+
+    async def _process_wait_message(self, message: TaskMessage):
+        await asyncio.sleep(message.duration)
+        return "Sleep..."
+
+    async def _process_ping_message(self):
+        return "Pong!"
+
+    async def _process_fail_message(self):
+        return "Fail!"
